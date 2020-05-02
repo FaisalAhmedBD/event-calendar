3857569d297a0bce3d3e5bc586606f78bcfdb02c
faisalahmedbd <faisalcse29@gmail.com>
2020-05-03 02:36:18 +0600

    Updates

---
+++ precache-manifest.b13d21d35f3c1a414e96498bd65dc1ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9f13bc7a1ff4fd67804",
+    "url": "/event-calendar/static/js/runtime~main.d9f13bc7.js"
+  },
+  {
+    "revision": "035d0acfee317fe026d7",
+    "url": "/event-calendar/static/js/main.035d0acf.chunk.js"
+  },
+  {
+    "revision": "c9720503efe391834ca4",
+    "url": "/event-calendar/static/js/1.c9720503.chunk.js"
+  },
+  {
+    "revision": "035d0acfee317fe026d7",
+    "url": "/event-calendar/static/css/main.1d13587e.chunk.css"
+  },
+  {
+    "revision": "c9720503efe391834ca4",
+    "url": "/event-calendar/static/css/1.3f8b58ff.chunk.css"
+  },
+  {
+    "revision": "6e023bae5934bef3542b1aa96535e083",
+    "url": "/event-calendar/index.html"
+  }
+];
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './button';
-export { default as Calendar } from './calendar';
-export { default as PageHeader } from './PageHeader';
-export { default as WeeklyDaysName } from './WeeklyDaysName';
