4955bc3edb102211e1ee5c400f72704a534febd4
faisalahmedbd <faisalcse29@gmail.com>
2020-07-18 23:36:42 +0600

    Updates

---
+++ precache-manifest.28202ee23fe3475714c35a52042bec37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949ae7a4fcf1828220ffe3e7f897a46a",
+    "url": "/event-calendar/index.html"
+  },
+  {
+    "revision": "d64d5666680e09845ed1",
+    "url": "/event-calendar/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b8e71280a7001dbd881",
+    "url": "/event-calendar/static/css/main.9bf34acc.chunk.css"
+  },
+  {
+    "revision": "d64d5666680e09845ed1",
+    "url": "/event-calendar/static/js/2.a88038c7.chunk.js"
+  },
+  {
+    "revision": "38a4b558739197dd9ec900c0561b7e3e",
+    "url": "/event-calendar/static/js/2.a88038c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8e71280a7001dbd881",
+    "url": "/event-calendar/static/js/main.8270b402.chunk.js"
+  },
+  {
+    "revision": "0f75cd2dffe54ee7e6a4",
+    "url": "/event-calendar/static/js/runtime-main.4f28ddeb.js"
+  }
+]);
